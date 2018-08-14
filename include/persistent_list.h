1d24f3bc39b67870a82685b024a10c91aed4dd8a
Davi Braga <davibrg@gmail.com>
2018-08-14 17:55:58 -0300

    added pmem

--- include/persistent_list.h
+++ include/persistent_list.h
@@ -7,63 +7,88 @@
 #include <libpmemobj++/pool.hpp>
 #include <libpmemobj++/transaction.hpp>
 #include <iostream>
-
-//explicit PersistentList(const char* path, const char* layout) {
-
-//}
-
-//~PersistentList() {
-//    try {
-//        pool_.close();
-//    }
-//    catch(const std::logic_error& excep) {
-//        std::cout << excep.what() << std::endl;
-//        std::abort;
-//    }
-//}
+#include <unordered_map>
+#include <vector>
 
 
 template <typename T>
 struct ListNode {
         pmem::obj::persistent_ptr<ListNode<T>> next;
-        pmem::obj::persistent_ptr<ListNode<T>> previous;
-        pmem::obj::p<uint64_t> key;
+        pmem::obj::persistent_ptr<ListNode<T>> prev;
+        pmem::obj::p<uint64_t> id;
         pmem::obj::p<T> obj;
 };
-
-
-
 
 template <typename T>
 class PersistentList {
 
 public:
 
-    ListNode<T>* Push(pmem::obj::pool_base &pool, uint64_t key, const  T &value) {
-        pmem::obj::transaction::exec_tx(pool, [&] {
-            auto current = pmem::obj::make_persistent<ListNode<T>>();
-            current->key = key;
-            current->value = value;
-            if (head_ == nullptr) {
-                head_ = current;
-            }
-            else {
-                current->next = head_;
-                head_->previous = current;
-                head_ = current;
+    void Commit(pmem::obj::pool_base &pool,
+                const std::vector<T> &intention_list,
+                std::unordered_map<uint64_t,
+                pmem::obj::persistent_ptr<ListNode<T>>> &lookup_table) {
+
+        pmem::obj::transaction::exec_tx(pool, [&](){
+
+            for (auto &entry : intention_list) {
+                auto it = lookup_table.find(entry.id);
+                if (it == std::end(lookup_table)) {
+                    auto ptr = AddNewEntry(entry);
+                    lookup_table[entry.id] = ptr;
+                }
+                else {
+                    it->second->obj = entry;
+                }
             }
         });
-        return head_.get();
     }
 
+    void Print() {
+        auto current = head_;
+        while (current != nullptr) {
+            std::cout << current->id << " ";
+            current = current->next;
+        }
+        std::cout << std::endl;
+    }
 
-    bool IsEmpty() {
-        return (head_ == nullptr);
+    void RecoverNVM(pmem::obj::pool_base &pool,
+                    std::unordered_map<uint64_t,T> &data_base,
+                    std::unordered_map<uint64_t,
+                    pmem::obj::persistent_ptr<ListNode<T>>> &lookup_table) {
+        auto current = head_;
+        while (current != nullptr) {
+            data_base.insert(std::make_pair<uint64_t,T>(
+                                 current->obj.get_ro().id, current->obj.get_ro()));
+            lookup_table.insert(std::make_pair<uint64_t,
+                                pmem::obj::persistent_ptr<ListNode<T>>>>(
+                                    current->obj.get_ro().id, current));
+            current = current->next;
+        }
     }
 
 
 private:
+    pmem::obj::persistent_ptr<ListNode<T>>
+    AddNewEntry(const T &entry) {
+        auto entry_node = pmem::obj::make_persistent<ListNode<T>>();
+        entry_node->id = entry.id;
+        entry_node->obj = entry;
+        if (head_ == nullptr) {
+            head_ = tail_ = entry_node;
+        }
+        else {
+            entry_node->next = head_;
+            head_->prev = entry_node;
+            head_ = entry_node;
+        }
+
+        return entry_node;
+    }
+
     pmem::obj::persistent_ptr<ListNode<T>> head_;
+    pmem::obj::persistent_ptr<ListNode<T>> tail_;
 };
 
 #endif // PERSISTENTLIST_H
--- src/main.cpp
+++ src/main.cpp
@@ -2,30 +2,62 @@
 
 #include "persistent_list.h"
 
+struct Tuple {
+    uint64_t id;
+    char data[500];
+};
+
 int main(int argc, char *argv[]) {
 
     const char* path = "/mnt/mem/pmem_content";
     const char* layout = "queue";
 
-    pmem::obj::pool<PersistentList<uint64_t>> pool;
+    pmem::obj::pool<PersistentList<Tuple>> pool;
+
+
+    size_t pool_size = 4000000000;
+
+    bool first_time = false;
+
 
     if (access(path, F_OK) != 0) {
-        pool = pmem::obj::pool<PersistentList<uint64_t>>::create(
-                                                path, layout,
-                                                PMEMOBJ_MIN_POOL,
+        pool = pmem::obj::pool<PersistentList<Tuple>>::create(
+                                                path, layout, pool_size,
                                                 S_IWUSR | S_IRUSR);
-        } else {
-        pool = pmem::obj::pool<PersistentList<uint64_t>>::open(path, layout);
+        first_time = true;
+        }
+    else {
+        pool = pmem::obj::pool<PersistentList<Tuple>>::open(path, layout);
+        first_time = false;
     }
 
-    auto list = pool.get_root();
+    auto recovery_manager = pool.get_root();
 
-    if (list->IsEmpty()) {
-        std::cout << "Empty!" << std::endl;
+    std::unordered_map<uint64_t,
+                    pmem::obj::persistent_ptr<ListNode<Tuple>>> lookup_table;
+    std::unordered_map<uint64_t, Tuple> data_base;
+
+
+    if (first_time) {
+        Tuple t = {0};
+        std::vector<Tuple> intention_list;
+        intention_list.push_back(t);
+        intention_list.push_back(t);
+        intention_list.push_back(t);
+
+        for (uint64_t i = 0; i < 20; i +=3) {
+            intention_list[0].id = i;
+            intention_list[1].id = i+1;
+            intention_list[2].id = i+2;
+            recovery_manager->Commit(pool, intention_list, lookup_table);
+            std::cout << i << std::endl;
+        }
     }
     else {
-        std::cout << "Not empty!" << std::endl;
+        recovery_manager->RecoverNVM(pool, data_base, lookup_table);
     }
+
+
 
     pool.close();
 
